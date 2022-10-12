d7aabeecb54c223e039414b14ca8f7146e8ad2d1
Samuel Armbrust <samuel.armbrust@sap.com>
2022-10-12 15:07:45 -0300

    adding graphs and small change in evaluate method

--- evaluate.py
+++ evaluate.py
@@ -7,10 +7,13 @@ def get_hottest_point(heatmap):
     w, h = heatmap.shape
     flattened_heatmap = np.ndarray.flatten(heatmap)
     hottest_idx = np.argmax(flattened_heatmap)
-    return np.flip(np.array(np.unravel_index(hottest_idx, [w, h])))
+    var_to_return = np.flip(np.array(np.unravel_index(hottest_idx, [w, h])))
+    plt.imshow(var_to_return, cmap='gray')
+    return var_to_return
 
 
 def get_mode_probability(heatmap):
+    plt.imshow(heatmap, cmap='gray')
     return np.max(heatmap)
 
 
